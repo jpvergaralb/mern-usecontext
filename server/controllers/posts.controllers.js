93c7dac328c8a7775b5cfbe2989ba6c87bfe4149
jpvergaralb <jpvergaralb@uc.cl>
2023-02-16 05:49:54 -0300

    basic controllers/db logic

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,19 +1,65 @@
+import Post from "../models/Post.js";
+
 export const getPosts = async (req, res) => {
-  res.status(200).json("get posts")
-}
+  const posts = await Post.find();
+  res.status(200).json(posts);
+};
 
 export const getPost = async (req, res) => {
-  res.status(200).json("get post")
-}
+  const id = req.params.id
+  try {
+    const post = await Post.findById(id)
+    if (!post) return res.status(404).json(`Post of id ${id} not found`);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
 
 export const createPost = async (req, res) => {
-  res.status(200).json("create post")
-}
+  const { title, description, image } = req.body;
+  if (!title || !description)
+    return res.status(400).json("Missing fields: title or description.");
+  try {
+    const post = new Post({ title, description });
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
 
 export const updatePost = async (req, res) => {
-  res.status(200).json("update post")
-}
+  const { title, description, image } = req.body;
+  const id = req.params.id;
+  if (!title && !description && !image)
+    return res
+      .status(400)
+      .json("Missing fields: at least update one attribute.");
+  try {
+    const post = await Post.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        image,
+      },
+      { new: true }
+    );
+    if (!post) return res.status(404).json(`Post of id ${id} not found`);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
 
 export const deletePost = async (req, res) => {
-  res.status(200).json("delete post")
-}
+  const id = req.params.id;
+  try {
+    const post = await Post.findByIdAndDelete(id, { new: true });
+    if (!post) return res.status(404).json(`Post of id ${id} not found`);
+    res.status(204).json({ deleted: post });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
