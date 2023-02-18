9dc5a7330856fc9cb81937e65b52feece4f3ba59
jpvergaralb <jpvergaralb@uc.cl>
2023-02-18 05:46:49 -0300

    add fileupload middleware

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 import express, { json } from "express";
 import { postRouter } from "./routes/posts.routes.js";
+import fileUpload from "express-fileupload";
 
 export const app = express();
+
+//middlewares
 app.use(express.json())
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: './upload'
+}))
+
+
 app.use(postRouter)
