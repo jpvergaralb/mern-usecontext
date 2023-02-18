ddd50a66615dda337760df6f9d0ac0492589e90e
jpvergaralb <jpvergaralb@uc.cl>
2023-02-18 05:51:36 -0300

    add new env variables:

--- server/config.js
+++ server/config.js
@@ -3,3 +3,6 @@ dotenv.config()
 
 export const MONGODB_URI_PASSWORD = process.env.MONGODB_URI_PASSWORD || "mongodb://localhost/db"
 export const PORT = process.env.PORT || 3000
+export const CLOUNDARY_CLOUD_NAME = process.env.CLOUNDARY_CLOUD_NAME
+export const CLOUNDARY_API_KEY = process.env.CLOUNDARY_API_KEY
+export const CLOUNDARY_API_SECRET = process.env.CLOUNDARY_API_SECRET
