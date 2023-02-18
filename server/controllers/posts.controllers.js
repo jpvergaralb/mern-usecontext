52c1f6355cf61f941e30f2a9cbe854b24960043c
jpvergaralb <jpvergaralb@uc.cl>
2023-02-18 05:52:00 -0300

    add upload and delete image logic | cloudinary

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,8 +1,17 @@
 import Post from "../models/Post.js";
+import { uploadImage } from "../libs/cloudinary.js";
+import { deleteImage } from "../libs/cloudinary.js";
+import fs from 'fs-extra'
 
 export const getPosts = async (req, res) => {
-  const posts = await Post.find();
-  res.status(200).json(posts);
+  try {
+    const posts = await Post.find();
+    if (!posts) return res.status(404).json(`Posts not found.`);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+  
 };
 
 export const getPost = async (req, res) => {
@@ -17,13 +26,22 @@ export const getPost = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const { title, description, image } = req.body;
+  const { title, description } = req.body;
+  let image = null
   if (!title || !description)
     return res.status(400).json("Missing fields: title or description.");
   try {
-    const post = new Post({ title, description });
+    if (req.files) {
+      const response = await uploadImage(req.files.image.tempFilePath)
+      image = {
+        url: response.secure_url,
+        public_id: response.public_id
+      }
+      await fs.remove(req.files.image.tempFilePath)
+    }
+    const post = new Post({ title, description, image });
     await post.save();
-    res.status(200).json(post);
+    res.status(201).json(post);
   } catch (error) {
     res.status(500).json(error.message);
   }
@@ -58,7 +76,11 @@ export const deletePost = async (req, res) => {
   try {
     const post = await Post.findByIdAndDelete(id, { new: true });
     if (!post) return res.status(404).json(`Post of id ${id} not found`);
-    res.status(204).json({ deleted: post });
+    if (post.image) {
+      console.log("entro")
+      await deleteImage(post.image.public_id)
+    }
+    res.status(200).json({ deleted: post });
   } catch (error) {
     return res.status(500).json(error.message);
   }
